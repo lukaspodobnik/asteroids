ec3105555993e69072911919fd69688ad731814b
lukaspodobnik <lukas.podobnik@gmail.com>
2025-07-12 21:35:02 +0200

    Add player with movement; use pygame-Groups to manage sprites

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
+            
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 
